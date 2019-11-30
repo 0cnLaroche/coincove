42678cd4ec66d85650483835d4f2edd5fbf6792a
Samuel Laroche <samuellaroche@live.ca>
2019-11-30 18:45:07 -0500

    removed config.properties

--- webserver/src/main/java/webserver/config/Config.java
+++
@@ -1,9 +0,0 @@
-package webserver.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:config.properties")
-public class Config {
-}
